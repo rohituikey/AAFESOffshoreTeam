590052f03a3b83802d5b0c3b13d169e601ee18aa
shganji <shganji@LNAR-1P6FMC2.corp.capgemini.com>
2017-05-23 19:56:07 -0500

    Added Token Solution

--- TokenService/src/main/java/com/aafes/tokenizer/LookUpService.java
+++ TokenService/src/main/java/com/aafes/tokenizer/LookUpService.java
@@ -5,7 +5,12 @@
  */
 package com.aafes.tokenizer;
 
+import com.aafes.tokenservice.util.AccountType;
+import generated.AccountTypeType;
 import generated.TokenMessage;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
@@ -15,12 +20,41 @@ public class LookUpService {
     
     @EJB
     private VaultDao vaultDao;
+    @EJB
+    private TokenizerDao tokenizerDao;
     
-    public String process(TokenMessage tm)
+    public String process(TokenMessage tm) throws ParseException
     {
         String accountNumber = "";
+        Vault vault = null;
         
-        Vault vault = vaultDao.find(tm.getRequest().getAccount());
+        if(tm.getRequest().getAccountType().value().equalsIgnoreCase(AccountType.TOKEN))
+        {
+           TokenBank tokenBank = tokenizerDao.find(tm.getRequest().getAccount(), tm.getRequest().getTokenBankName());
+           if(tokenBank != null)
+           {
+               SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd");
+               Date expDate = df.parse(tokenBank.getExpirydate());;
+               String date = df.format(new Date());
+               Date currentDate = df.parse(date);
+               
+               if(currentDate.after(expDate))
+               {
+                   tokenBank.setStatus(TokenizerConstants.EXPIRED);
+                   tokenizerDao.save(tokenBank);
+                   return accountNumber;
+               }
+               
+           }
+        } 
+        
+        if(tm.getRequest().getAccountType().value().equalsIgnoreCase(AccountType.PAN))
+        {
+            vault = vaultDao.findByAccount(tm.getRequest().getAccount());
+        } else {
+            vault = vaultDao.findByToken(tm.getRequest().getAccount());
+        }
+        
         
         if(vault != null)
         {
--- TokenService/src/main/java/com/aafes/tokenizer/Tokenizer.java
+++ TokenService/src/main/java/com/aafes/tokenizer/Tokenizer.java
@@ -5,11 +5,12 @@
  */
 package com.aafes.tokenizer;
 
+import com.aafes.tokenservice.util.AccountType;
+import com.aafes.tokenservice.util.ResponseType;
 import generated.AccountTypeType;
 import generated.TokenMessage;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
-
 
 @Stateless
 public class Tokenizer {
@@ -18,55 +19,65 @@ public class Tokenizer {
     private LookUpService lookUpService;
     @EJB
     private IssueService issueService;
+    @EJB
+    private Configurator configurator;
 
     public TokenMessage handle(TokenMessage tm) {
 
+        TokenMessage.Response response = new TokenMessage.Response();
         try {
 
             if (tm.getRequest() != null) {
                 TokenMessage.Request request = tm.getRequest();
-                TokenMessage.Response response = new TokenMessage.Response();
-                if (request.getRequestType().value().equalsIgnoreCase(TokenizerConstants.LOOKUP)) {
+
+                if (request.getRequestType().value().equalsIgnoreCase(TokenizerConstants.LOOKUP)
+                        && request.getAccountType().value().equalsIgnoreCase(AccountType.TOKEN)) {
                     String account = lookUpService.process(tm);
-                    
+
                     if (account != null && !account.trim().isEmpty()) {
                         response.setAccountType(AccountTypeType.PAN);
                         response.setAccount(account);
                         response.setDescriptionField("Success");
-                        response.setResponseType("SUCCESS");
-                        response.setReasonCode("100");
+                        response.setResponseType(ResponseType.SUCCESS);
+                        response.setReasonCode(configurator.get("SUCCESS"));
                     } else {
-                        
-                        response.setDescriptionField("INVALID TOKEN");
-                        response.setResponseType("FAILURE");
-                        response.setReasonCode("900");
+
+                        response.setDescriptionField("INVALID_TOKEN");
+                        response.setResponseType(ResponseType.FAILED);
+                        response.setReasonCode(configurator.get("INVALID_TOKEN"));
                     }
 
-                } else if (request.getRequestType().value().equalsIgnoreCase(TokenizerConstants.ISSUE)) {
+                } else if (request.getRequestType().value().equalsIgnoreCase(TokenizerConstants.ISSUE)
+                        && request.getAccountType().value().equalsIgnoreCase(AccountType.PAN)) {
                     String token = issueService.process(tm);
-                   
+
                     if (token != null && !token.trim().isEmpty()) {
                         response.setAccountType(AccountTypeType.TOKEN);
                         response.setAccount(token);
                         response.setDescriptionField("Success");
-                        response.setResponseType("SUCCESS");
-                        response.setReasonCode("100");
+                        response.setResponseType(ResponseType.SUCCESS);
+                        response.setReasonCode(configurator.get("SUCCESS"));
                     } else {
-                        
-                        response.setDescriptionField("INVALID CARD NUMBER");
-                        response.setResponseType("FAILURE");
-                        response.setReasonCode("901");
+
+                        response.setDescriptionField("INVALID_CARD_NUMBER");
+                        response.setResponseType(ResponseType.FAILED);
+                        response.setReasonCode(configurator.get("INVALID_CARD_NUMBER"));
                     }
+                } else {
+                    response.setDescriptionField("BAD_REQUEST");
+                    response.setResponseType(ResponseType.FAILED);
+                    response.setReasonCode(configurator.get("BAD_REQUEST"));
                 }
-                
-                tm.setResponse(response);
 
             }
 
         } catch (Exception e) {
-
+            response.setDescriptionField("INTERNAL_SERVER_ERROR");
+            response.setResponseType(ResponseType.FAILED);
+            response.setReasonCode(configurator.get("INTERNAL_SERVER_ERROR"));
         }
 
+        tm.setResponse(response);
         return tm;
     }
 }

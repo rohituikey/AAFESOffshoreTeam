23a3924a8b2ea1802acb2530dc69d6e1a3d04dc6
singha <singha@EC2AMAZ-86NUPR5.corp.aafes.stargate.com>
2017-06-27 13:45:54 +0530

    Code check in for FDMS Timeout Reversal functionality.

--- StarGate/src/main/java/com/aafes/stargate/gateway/fdms/CompassGateway.java
+++ StarGate/src/main/java/com/aafes/stargate/gateway/fdms/CompassGateway.java
@@ -2,15 +2,17 @@ package com.aafes.stargate.gateway.fdms;
 
 import com.aafes.stargate.gateway.GatewayException;
 import com.aafes.stargate.authorizer.entity.Transaction;
+import com.aafes.stargate.control.Configurator;
 import com.aafes.stargate.gateway.Gateway;
 import com.aafes.stargate.util.RequestType;
 import com.aafes.stargate.util.ResponseType;
-import javax.annotation.Resource;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import javax.ejb.EJB;
-import javax.ejb.SessionContext;
 import javax.ejb.Stateless;
-import javax.ejb.Timeout;
-import javax.ejb.Timer;
 import org.slf4j.LoggerFactory;
 
 @Stateless
@@ -22,30 +24,26 @@ public class CompassGateway   extends Gateway {
     @EJB
     private CompassGatewayProcessor cgp;
     
-    @Resource
-    private SessionContext context;
+//    
+//    @Resource
+//    private SessionContext context;
+    
+    @EJB
+    private Configurator configurator;
+    
     
     private Transaction transaction = new Transaction();
     
 
-    public void createTimer(long duration) {
-      context.getTimerService().createTimer(duration, "My timer");
-      System.out.println("timer");
-   }
+
     
-     @Timeout
-     public void timeout(Timer timer) {
-         
-        // processMessage(transaction);
-         
-        Transaction localTransaction = new Transaction();
-        localTransaction = transaction;
-        localTransaction.setRequestType(RequestType.REVERSAL);
-        transaction = cgp.execute(localTransaction);
-        transaction.setResponseType(ResponseType.TIMEOUT);
-        transaction.setDescriptionField("Time out occured");
-        transaction.setReasonCode("TIMEOUT OCCURED");
-     }
+    
+    
+    
+    
+    
+    
+    
     
     @Override
     public Transaction processMessage(Transaction t) {
@@ -53,10 +51,23 @@ public class CompassGateway   extends Gateway {
         try {
             this.validateTransaction(t);
             if (cgp != null) {
-                 createTimer(25000);
-                    //t.setRequestAuthDateTime(ResponseType.TIMEOUT);
-                t = cgp.execute(t);
-                
+                final ExecutorService service = Executors.newSingleThreadExecutor();
+                try {
+                    final Future<?> f = service.submit(() -> {
+                        transaction= cgp.execute(t);
+                    });
+                    f.get(25, TimeUnit.SECONDS);
+                } catch (final TimeoutException e) {
+                    transaction.setRequestType(RequestType.REVERSAL);
+                    transaction = cgp.execute(transaction);
+                    transaction.setResponseType(ResponseType.TIMEOUT);
+                    transaction.setDescriptionField("Time out occured");
+                    transaction.setReasonCode(configurator.get("TIMEOUT_EXCEPTION"));
+                } catch (final Exception e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    service.shutdown();
+                }
             } else {
                 t.setResponseType(ResponseType.DECLINED);
                 t.setDescriptionField("INTERNAL SERVER ERROR");
--- StarGate/src/test/java/com/aafes/stargate/test/FDMSTimeoutTest.java
+++ StarGate/src/test/java/com/aafes/stargate/test/FDMSTimeoutTest.java
@@ -10,29 +10,29 @@ import com.aafes.stargate.gateway.fdms.CompassGateway;
 import com.aafes.stargate.gateway.fdms.CompassGatewayProcessor;
 import com.aafes.stargate.util.MediaType;
 import com.aafes.stargate.util.StrategyType;
+import static jdk.nashorn.internal.objects.NativeRegExp.test;
 import org.junit.Assert;
+import org.junit.Test;
 
 /**
  *
  * @author singha
  */
 public class FDMSTimeoutTest {
-    
-    
-    
-     private Transaction transaction = new Transaction();   
-     public void testfdmsTimeout()
-     {
-         CompassGateway compassGateway = new CompassGateway();
-         CompassGatewayProcessor compassGatewayProcessor = new CompassGatewayProcessor();
-         compassGateway.setCgp(compassGatewayProcessor);
-         transaction.setMedia(MediaType.AMEX);
-         transaction.setStrategy(StrategyType.ECOMM);
-         transaction.setIdentityUuid("c47e4366-fe72-473b-b114-523e8de5641f");
-         Transaction result =compassGateway.processMessage(transaction);
-         Assert.assertEquals("000", result.getReasonCode());
-         
-     }
-    
-    
+
+    private Transaction transaction = new Transaction();
+
+    @Test
+    public void testfdmsTimeout() {
+        CompassGateway compassGateway = new CompassGateway();
+        CompassGatewayProcessor compassGatewayProcessor = new CompassGatewayProcessor();
+        compassGateway.setCgp(compassGatewayProcessor);
+        transaction.setMedia(MediaType.AMEX);
+        transaction.setStrategy(StrategyType.ECOMM);
+        transaction.setIdentityUuid("c47e4366-fe72-473b-b114-523e8de5641f");
+        Transaction result = compassGateway.processMessage(transaction);
+        Assert.assertEquals("000", result.getReasonCode());
+
+    }
+
 }

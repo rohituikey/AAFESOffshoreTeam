0d4248b9601392d54969d80bfa004be95ea40da9
uikuyr <uikuyr@EC2AMAZ-SKF6SHG.corp.aafes.stargate.com>
2017-08-02 18:52:01 +0530

    Functionality for converting ISO format to response object.

--- StarGate/src/main/java/com/aafes/stargate/gateway/wex/NBSRequestGenerator.java
+++ StarGate/src/main/java/com/aafes/stargate/gateway/wex/NBSRequestGenerator.java
@@ -6,12 +6,13 @@
 package com.aafes.stargate.gateway.wex;
 
 import com.aafes.nbslogonrequestschema.NbsLogonRequest;
+import com.aafes.nbsresponseacknowledgmentschema.ResponseAcknowlegment;
+import com.aafes.stargate.util.ResponseType;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.jpos.iso.ISOException;
 import org.jpos.iso.ISOMsg;
 import org.jpos.iso.packager.GenericPackager;
-
 
 /**
  *
@@ -21,40 +22,41 @@ public class NBSRequestGenerator {
 
     private String iso8583Format;
     private int promptCountIndex;
-    private ISOMsg request;
+    private ISOMsg isoMsg;
     private GenericPackager packager;
+    private ResponseAcknowlegment responseAcknowlegment;
 
     public String generateLogOnPacketRequest(NbsLogonRequest nbsLogonRequest) {
 
         try {
             packager = new GenericPackager("src/main/resources/xml/NBSLogonPackager.xml");
 
-            request = new ISOMsg();
-            request.setPackager(packager);
-            request.setMTI("0231");
-            request.set(10, nbsLogonRequest.getTermId());
-            request.set(12, nbsLogonRequest.getAppName());
-            request.set(13, nbsLogonRequest.getAppVersion().toString());
-            request.set(14, nbsLogonRequest.getTimeZone().toString());
-            request.set(15, nbsLogonRequest.getHeaderRecord().getA());
-            request.set(16, nbsLogonRequest.getHeaderRecord().getKey().toString());
-            request.set(17, nbsLogonRequest.getHeaderRecord().getTransType().toString());
-            request.set(18, nbsLogonRequest.getHeaderRecord().getCardType());
-            request.set(19, nbsLogonRequest.getHeaderRecord().getCATFlag().toString());
-            request.set(110, nbsLogonRequest.getHeaderRecord().getPumpNo());
-            request.set(111, nbsLogonRequest.getHeaderRecord().getServiceType().toString());
-            request.set(112, nbsLogonRequest.getHeaderRecord().getTrack().toString());
-            request.set(113, nbsLogonRequest.getHeaderRecord().getCardSpecificData().getAcctInfo());
-            request.set(114, nbsLogonRequest.getHeaderRecord().getCardSpecificData().getAmount().toString());
-//            request.set(15, nbsLogonRequest.getHeaderRecord().getCardSpecificData().getWexPromptDetails().getPromptDetailCount().toString());
+            isoMsg = new ISOMsg();
+            isoMsg.setPackager(packager);
+            isoMsg.setMTI("0231");
+            isoMsg.set(10, nbsLogonRequest.getTermId());
+            isoMsg.set(12, nbsLogonRequest.getAppName());
+            isoMsg.set(13, nbsLogonRequest.getAppVersion().toString());
+            isoMsg.set(14, nbsLogonRequest.getTimeZone().toString());
+            isoMsg.set(15, nbsLogonRequest.getHeaderRecord().getA());
+            isoMsg.set(16, nbsLogonRequest.getHeaderRecord().getKey().toString());
+            isoMsg.set(17, nbsLogonRequest.getHeaderRecord().getTransType().toString());
+            isoMsg.set(18, nbsLogonRequest.getHeaderRecord().getCardType());
+            isoMsg.set(19, nbsLogonRequest.getHeaderRecord().getCATFlag().toString());
+            isoMsg.set(110, nbsLogonRequest.getHeaderRecord().getPumpNo());
+            isoMsg.set(111, nbsLogonRequest.getHeaderRecord().getServiceType().toString());
+            isoMsg.set(112, nbsLogonRequest.getHeaderRecord().getTrack().toString());
+            isoMsg.set(113, nbsLogonRequest.getHeaderRecord().getCardSpecificData().getAcctInfo());
+            isoMsg.set(114, nbsLogonRequest.getHeaderRecord().getCardSpecificData().getAmount().toString());
+//            isoMsg.set(15, nbsLogonRequest.getHeaderRecord().getCardSpecificData().getWexPromptDetails().getPromptDetailCount().toString());
 //            for (promptCountIndex=16 ; promptCountIndex  <  (nbsLogonRequest.getHeaderRecord().getCardSpecificData().getWexPromptDetails().getPromptDetailCount().getPromptTypeOrPromptValue().size()); promptCountIndex++) {
-//                 request.set(promptCountIndex, (nbsLogonRequest.getHeaderRecord().getCardSpecificData().getWexPromptDetails().getPromptDetailCount().getPromptTypeOrPromptValue().get(promptCountIndex)).toString());
+//                 isoMsg.set(promptCountIndex, (nbsLogonRequest.getHeaderRecord().getCardSpecificData().getWexPromptDetails().getPromptDetailCount().getPromptTypeOrPromptValue().get(promptCountIndex)).toString());
 //            }
-//            request.set(promptCountIndex+1, nbsLogonRequest.getHeaderRecord().getCardSpecificData().getWexProductDetails().getProdDetailCount().toString());
+//            isoMsg.set(promptCountIndex+1, nbsLogonRequest.getHeaderRecord().getCardSpecificData().getWexProductDetails().getProdDetailCount().toString());
 //            for (promptCountIndex=promptCountIndex+2 ; promptCountIndex  <  (nbsLogonRequest.getHeaderRecord().getCardSpecificData().getWexProductDetails().getProdDetailCount().getPriceOrQuantityOrProductCode().size()); promptCountIndex++) {
-//                 request.set(promptCountIndex, (nbsLogonRequest.getHeaderRecord().getCardSpecificData().getWexProductDetails().getProdDetailCount().getPriceOrQuantityOrProductCode().get(promptCountIndex)).toString());
+//                 isoMsg.set(promptCountIndex, (nbsLogonRequest.getHeaderRecord().getCardSpecificData().getWexProductDetails().getProdDetailCount().getPriceOrQuantityOrProductCode().get(promptCountIndex)).toString());
 //            }
-            byte[] data = request.pack();
+            byte[] data = isoMsg.pack();
             iso8583Format = new String(data);
             System.out.println("output for NBS Iso 8583 format= " + iso8583Format);
             return iso8583Format;
@@ -62,6 +64,27 @@ public class NBSRequestGenerator {
             Logger.getLogger(NBSRequestGenerator.class.getName()).log(Level.SEVERE, null, ex);
         }
         return iso8583Format;
+    }
+
+    public ResponseAcknowlegment unmarshalResponseAcknowledgment(String response) {
+
+        try {
+            isoMsg = new ISOMsg();
+            GenericPackager genericPackager = new GenericPackager("src/main/resources/xml/ResponseAcknowledgment.xml");
+            isoMsg.setPackager(genericPackager);
+            isoMsg.unpack(response.getBytes());
+            if(isoMsg.getString(10).trim().equalsIgnoreCase("c$"))
+            responseAcknowlegment.setResponseType(ResponseType.APPROVED);
+            else if(isoMsg.getString(10).trim().equalsIgnoreCase("c?"))
+            responseAcknowlegment.setResponseType(ResponseType.DECLINED);
+            responseAcknowlegment.setReasonCode(isoMsg.getString(11));
+            return responseAcknowlegment;
+        } catch (ISOException ex) {
+            Logger.getLogger(NBSRequestGenerator.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex){
+            Logger.getLogger(NBSRequestGenerator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return responseAcknowlegment;
     }
 
     public String getIso8583Format() {
@@ -80,12 +103,12 @@ public class NBSRequestGenerator {
         this.promptCountIndex = promptCountIndex;
     }
 
-    public ISOMsg getRequest() {
-        return request;
+    public ISOMsg getIsoMsg() {
+        return isoMsg;
     }
 
-    public void setRequest(ISOMsg request) {
-        this.request = request;
+    public void setIsoMsg(ISOMsg isoMsg) {
+        this.isoMsg = isoMsg;
     }
 
     public GenericPackager getPackager() {
@@ -95,5 +118,15 @@ public class NBSRequestGenerator {
     public void setPackager(GenericPackager packager) {
         this.packager = packager;
     }
+
+    public ResponseAcknowlegment getResponseAcknowlegment() {
+        return responseAcknowlegment;
+    }
+
+    public void setResponseAcknowlegment(ResponseAcknowlegment responseAcknowlegment) {
+        this.responseAcknowlegment = responseAcknowlegment;
+    }
+    
+    
 
 }
--- StarGate/src/test/java/com/aafes/stargate/gateway/wex/NBSRequestGeneratorTest.java
+++ StarGate/src/test/java/com/aafes/stargate/gateway/wex/NBSRequestGeneratorTest.java
@@ -4,7 +4,11 @@
  * and open the template in the editor.
  */
 package com.aafes.stargate.gateway.wex;
+
 import com.aafes.nbslogonrequestschema.NbsLogonRequest;
+import com.aafes.nbsresponseacknowledgmentschema.ResponseAcknowlegment;
+import com.aafes.stargate.gateway.wex.simulator.NBSClient;
+import com.aafes.stargate.util.ResponseType;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.logging.Level;
@@ -12,19 +16,21 @@ import java.util.logging.Logger;
 import org.jpos.iso.ISOException;
 import org.jpos.iso.ISOMsg;
 import org.jpos.iso.packager.GenericPackager;
+import org.junit.Assert;
+import static org.junit.Assert.fail;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
  * @author uikuyr
  */
 public class NBSRequestGeneratorTest {
-    
+
     NbsLogonRequest nbsLogonRequest;
     NBSRequestGenerator subjectUnderTest = new NBSRequestGenerator();
-    
+    NBSClient nBSClient = new NBSClient();
+
     @Before
     public void setUp() {
         nbsLogonRequest = new NbsLogonRequest();
@@ -34,10 +40,10 @@ public class NBSRequestGeneratorTest {
 //        NbsLogonRequest.HeaderRecord.CardSpecificData.WexPromptDetails wexPromptDetails= new NbsLogonRequest.HeaderRecord.CardSpecificData.WexPromptDetails();
 //        NbsLogonRequest.HeaderRecord.CardSpecificData.WexProductDetails.ProdDetailCount prodDetailCount = new NbsLogonRequest.HeaderRecord.CardSpecificData.WexProductDetails.ProdDetailCount();
 //        NbsLogonRequest.HeaderRecord.CardSpecificData.WexPromptDetails.PromptDetailCount promptDetailCount= new NbsLogonRequest.HeaderRecord.CardSpecificData.WexPromptDetails.PromptDetailCount();
-        
+
         cardSpecificData.setAcctInfo("dummy Info");
         cardSpecificData.setAmount(BigDecimal.ONE);
-        
+
         headerRecord.setA("dummy Info");
         headerRecord.setCATFlag(BigInteger.ONE);
         headerRecord.setCardType("dummy Info");
@@ -47,34 +53,43 @@ public class NBSRequestGeneratorTest {
         headerRecord.setTrack(BigInteger.ONE);
         headerRecord.setTransType(BigInteger.ONE);
         headerRecord.setCardSpecificData(cardSpecificData);
-        
+
         nbsLogonRequest.setAppName("dummy Appname");
         nbsLogonRequest.setAppVersion(new BigInteger("1"));
         nbsLogonRequest.setTermId("dummy Term ID");
         nbsLogonRequest.setTimeZone(new BigInteger("02501"));
         nbsLogonRequest.setHeaderRecord(headerRecord);
-        
-        ISOMsg request = new ISOMsg();
-        try {
-            GenericPackager packager = new GenericPackager("src/main/resources/xml/NBSLogonPackager.xml");
-            request.setPackager(packager);
-        } catch (ISOException ex) {
-            Logger.getLogger(NBSRequestGeneratorTest.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        subjectUnderTest.setRequest(request);
+
+        ISOMsg iSOMsg = new ISOMsg();
+        subjectUnderTest.setIsoMsg(iSOMsg);
     }
-    
 
     @Test
     public void testGenerateLogOnPacketRequest() {
         System.out.println("generateLogOnPacketRequest");
-        
-        NBSRequestGenerator instance = new NBSRequestGenerator();
-        String expResult = "";
-        String result = instance.generateLogOnPacketRequest(nbsLogonRequest);
-        //assertEquals(expResult, result);
+        try {
+            
+            String result = subjectUnderTest.generateLogOnPacketRequest(nbsLogonRequest);
+            String response = nBSClient.generateResponse(result);
+            Assert.assertEquals("APPROVED", response);
+        } catch (Exception ex) {
+            fail(ex.getMessage());
+        }
     }
 
-   
-    
-}
+        @Test
+        public void testUnmarshalResponse() {
+        System.out.println("generateLogOnPacketRequest");
+            try {
+                ResponseAcknowlegment result = new ResponseAcknowlegment();
+                subjectUnderTest.setResponseAcknowlegment(result);
+                result = subjectUnderTest.unmarshalResponseAcknowledgment("02310060000000000000002c$003100");
+                //String response = nBSClient.generateResponse(result);
+                Assert.assertEquals(ResponseType.APPROVED, result.getResponseType());
+            } catch (Exception ex) {
+                fail(ex.getMessage());
+            }
+
+        }
+
+    }

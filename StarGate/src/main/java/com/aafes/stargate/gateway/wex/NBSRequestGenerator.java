abcdc0400afec873bf8c31fcb44af40d53b4e3fc
burangir <burangir@EC2AMAZ-18I59K6.corp.aafes.stargate.com>
2017-08-07 15:07:07 +0530

    Resolved the issue where properties from stargate.properties were not successfully fetched.

--- StarGate/src/main/java/com/aafes/stargate/gateway/wex/NBSRequestGenerator.java
+++ StarGate/src/main/java/com/aafes/stargate/gateway/wex/NBSRequestGenerator.java
@@ -8,6 +8,7 @@ package com.aafes.stargate.gateway.wex;
 import com.aafes.nbsresponse.NBSResponse;
 import com.aafes.nbsresponseacknowledgmentschema.ResponseAcknowlegment;
 import com.aafes.stargate.authorizer.entity.Transaction;
+import com.aafes.stargate.control.Configurator;
 import com.aafes.stargate.util.InputType;
 import com.aafes.stargate.util.RequestType;
 import com.aafes.stargate.util.ResponseType;
@@ -16,7 +17,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.inject.Inject;
+import javax.ejb.EJB;
 import org.jpos.iso.ISOException;
 import org.jpos.iso.ISOMsg;
 import org.jpos.iso.packager.GenericPackager;
@@ -36,29 +37,40 @@ public class NBSRequestGenerator {
     private ResponseAcknowlegment responseAcknowlegment;
     private NBSResponse nBSResponse;
     Transaction transaction = new Transaction();
-    @Inject
-    private String APPLICATION_NAME;
-    @Inject
-    private String APPLICATION_VERSION;
-    @Inject
-    private String DAYLIGHT_SAVINGS_TIME_AT_SITE_ONE;
-    @Inject
-    private String CAPTURE_ONLY_REQUEST;
-    @Inject
-    private String TRANS_TYPE_PRE_AUTH;
-    @Inject
-    private String TRANS_TYPE_FINAL_AND_SALE;
-    @Inject
-    private String TRANS_TYPE_REFUND;
-    @Inject
-    private String CARD_TYPE_WEX;
-    @Inject
-    private String SERVICE_TYPE;
-    @Inject
-    private String SESSION_TYPE_AUTH;
+    
+    private String applicationName;
+    private String applicationVersion;
+    private String daylightSavingsTimeAtSiteOne;
+    private String captureOnlyRequest;
+    private String sessionTypeAuth;
+    private String transTypePreAuth;
+    private String transTypeFinalAndSale;
+    private String transTypeRefund;
+    private String cardTypeWex;
+    private String serviceType;
+    
     private String SCHEMA_PATH = "";
+    @EJB
+    private Configurator configurator;
 
     public String generateLogOnPacketRequest(Transaction t) {
+        
+        if(configurator == null){
+            configurator = new Configurator();
+            configurator.postConstruct();
+        }
+        
+        applicationName = configurator.get("APPLICATION_NAME");
+        applicationVersion = configurator.get("APPLICATION_VERSION");
+        daylightSavingsTimeAtSiteOne = configurator.get("DAYLIGHT_SAVINGS_TIME_AT_SITE_ONE");
+        captureOnlyRequest = configurator.get("CAPTURE_ONLY_REQUEST");
+        sessionTypeAuth = configurator.get("SESSION_TYPE_AUTH");
+        transTypePreAuth = configurator.get("TRANS_TYPE_PRE_AUTH");
+        transTypeFinalAndSale = configurator.get("TRANS_TYPE_FINAL_AND_SALE");
+        transTypeRefund = configurator.get("TRANS_TYPE_REFUND");
+        cardTypeWex = configurator.get("CARD_TYPE_WEX");
+        serviceType = configurator.get("SERVICE_TYPE");
+        
         transaction = t;
         try {
                 SCHEMA_PATH = "src/main/resources/xml/NBSLogonPackager.xml";
@@ -72,24 +84,24 @@ public class NBSRequestGenerator {
             isoMsg.setPackager(packager);
             isoMsg.setMTI("0231");
             isoMsg.set(10, transaction.getTermId());
-            isoMsg.set(12, APPLICATION_NAME);
-            isoMsg.set(13, APPLICATION_VERSION);
+            isoMsg.set(12, applicationName);
+            isoMsg.set(13, applicationVersion);
             isoMsg.set(14, createDateFormat());
-            isoMsg.set(15, SESSION_TYPE_AUTH);
+            isoMsg.set(15, sessionTypeAuth);
             isoMsg.set(16, transaction.getTransactionId().substring(0, 4));
-            if (transaction.getRequestType().equalsIgnoreCase(RequestType.PREAUTH)) isoMsg.set(17, TRANS_TYPE_PRE_AUTH);
+            if (transaction.getRequestType().equalsIgnoreCase(RequestType.PREAUTH)) isoMsg.set(17, sessionTypeAuth);
             else if (transaction.getRequestType().equalsIgnoreCase(RequestType.FINAL_AUTH)
                     || transaction.getRequestType().equalsIgnoreCase(RequestType.SALE)) {
-                isoMsg.set(17, TRANS_TYPE_FINAL_AND_SALE);
+                isoMsg.set(17, transTypeFinalAndSale);
             }
-            else if (transaction.getRequestType().equalsIgnoreCase(RequestType.REFUND)) isoMsg.set(17, TRANS_TYPE_REFUND);
-            isoMsg.set(18, CARD_TYPE_WEX);
+            else if (transaction.getRequestType().equalsIgnoreCase(RequestType.REFUND)) isoMsg.set(17, transTypeRefund);
+            isoMsg.set(18, cardTypeWex);
             isoMsg.set(19, transaction.getCatFlag());
             isoMsg.set(110, transaction.getPumpNmbr());
-            isoMsg.set(111, SERVICE_TYPE);
+            isoMsg.set(111, serviceType);
 
             if (transaction.getRequestType().equals(RequestType.FINAL_AUTH)) {
-                isoMsg.set(15, CAPTURE_ONLY_REQUEST);
+                isoMsg.set(15, captureOnlyRequest);
                 isoMsg.set(112, Long.toString(transaction.getAmount()));
                 isoMsg.set(113, Long.toString(transaction.getAmtPreAuthorized()));
                 isoMsg.set(114, transaction.getTransactionId());
@@ -100,7 +112,7 @@ public class NBSRequestGenerator {
             if (transaction.getInputType().equalsIgnoreCase(InputType.SWIPED)) isoMsg.set(117, transaction.getTrack2());
             else if (transaction.getInputType().equalsIgnoreCase(InputType.KEYED))//isoMsg.set(113, transaction.getTrack2());//Track0 formatt
             if (!(transaction.getRequestType().equals(RequestType.FINAL_AUTH))) isoMsg.set(118, Long.toString(t.getAmount()));
-            if (TRANS_TYPE_FINAL_AND_SALE.equals(10) || TRANS_TYPE_REFUND.equals(30)) isoMsg.set(119, (t.getTransactionId() + t.getTermId()));
+            if (transTypeFinalAndSale.equals(10) || transTypeRefund.equals(30)) isoMsg.set(119, (t.getTransactionId() + t.getTermId()));
 
 //            isoMsg.set(15, nbsLogonRequest.getHeaderRecord().getCardSpecificData().getWexPromptDetails().getPromptDetailCount().toString());
 //            for (promptCountIndex=16 ; promptCountIndex  <  (nbsLogonRequest.getHeaderRecord().getCardSpecificData().getWexPromptDetails().getPromptDetailCount().getPromptTypeOrPromptValue().size()); promptCountIndex++) {
@@ -242,7 +254,7 @@ public class NBSRequestGenerator {
         Date date = new Date();
         String ts = dateFormat.format(date);
         //2017-08-03 09:31:54.316
-        ts = ts.substring(11, 13) + ts.substring(14, 16) + DAYLIGHT_SAVINGS_TIME_AT_SITE_ONE;
+        ts = ts.substring(11, 13) + ts.substring(14, 16) + daylightSavingsTimeAtSiteOne;
         return ts;
     }
     
@@ -304,5 +316,19 @@ public class NBSRequestGenerator {
 
     public void setResponseAcknowlegment(ResponseAcknowlegment responseAcknowlegment) {
         this.responseAcknowlegment = responseAcknowlegment;
+    }
+
+    /**
+     * @return the configurator
+     */
+    public Configurator getConfigurator() {
+        return configurator;
+    }
+
+    /**
+     * @param configurator the configurator to set
+     */
+    public void setConfigurator(Configurator configurator) {
+        this.configurator = configurator;
     }
 }

82c0ce9681572a5703aabacc38c04438feb98557
uikuyr <uikuyr@EC2AMAZ-SKF6SHG.corp.aafes.stargate.com>
2017-08-10 15:38:34 +0530

    teting req generator

--- StarGate/src/main/java/com/aafes/stargate/gateway/wex/simulator/NBSFormatter.java
+++ StarGate/src/main/java/com/aafes/stargate/gateway/wex/simulator/NBSFormatter.java
@@ -299,4 +299,86 @@ public class NBSFormatter {
         ts = ts.substring(11, 13) + ts.substring(14, 16) + daylightSavingsTimeAtSiteOne;
         return ts;
     }
+    
+    //uncomment for testing purpose
+//    public String generateNewResponse() {
+//        String logonResponse = logonResponse();
+//        String nbsResponse = nbsResponse();
+//        return logonResponse+nbsResponse;
+//    }
+//
+//    private String logonResponse() {
+//        try {
+//            MessageFactory mfact = ConfigParser.createFromClasspathConfig("NBSAcknowlegment.xml");
+//            mfact.setTraceNumberGenerator(new SimpleTraceGenerator((int) (System.currentTimeMillis() % 10000)));
+//            mfact.setAssignDate(true);
+//
+//            IsoMessage isoMessage = mfact.newMessage(0x100);
+//        if (true) {
+//            isoMessage.setValue(10, "c$",IsoType.ALPHA,2);
+//            isoMessage.setValue(11, "100",IsoType.ALPHA,3);
+//        } else {
+//            isoMessage.setValue(10, "c?",IsoType.ALPHA,2);
+//            isoMessage.setValue(11, "200",IsoType.ALPHA,3);
+//        }
+//        isoMessage.setCharacterEncoding(
+//                    "UTF-8");
+//            isoMessage.setBinaryBitmap(
+//                    true);
+//            byte[] data = isoMessage.writeData();
+//
+//            return new String(data);
+//        } catch (IOException ex) {
+//            //Logger.getLogger(NBSStubImpl.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        return null;
+//    }
+//    private String nbsResponse() {
+//        try {
+//            MessageFactory mfact = ConfigParser.createFromClasspathConfig("NBSResponse.xml");
+//            mfact.setTraceNumberGenerator(new SimpleTraceGenerator((int) (System.currentTimeMillis() % 10000)));
+//            mfact.setAssignDate(true);
+//
+//            IsoMessage isoMessage = mfact.newMessage(0x100);
+//        isoMessage.setValue(10, "A",IsoType.ALPHA,25);
+//        isoMessage.setValue(11, "0278",IsoType.NUMERIC,4);
+//
+//        isoMessage.setValue(12, "3170621071655",IsoType.ALPHA,13);
+//        isoMessage.setValue(13, "N",IsoType.ALPHA,1);
+//        if (true) {
+//            isoMessage.setValue(14, "00",IsoType.ALPHA,2);
+//            isoMessage.setValue(15, "Approved",IsoType.ALPHA,32);
+//        } else {
+//            isoMessage.setValue(14, "01",IsoType.ALPHA,2);
+//            isoMessage.setValue(15, "Declined",IsoType.ALPHA,32);
+//        }
+//        isoMessage.setValue(16, "WEX",IsoType.ALPHA,4);
+//        isoMessage.setValue(17, "",IsoType.ALPHA,6);
+//        isoMessage.setValue(18, "",IsoType.ALPHA,7);
+//        isoMessage.setValue(19, "",IsoType.ALPHA,4);
+//        isoMessage.setValue(20, "",IsoType.ALPHA,4);
+//        isoMessage.setValue(21, "",IsoType.ALPHA,4);
+//        isoMessage.setValue(22, "5",IsoType.ALPHA,2);
+//        isoMessage.setValue(23, "0",IsoType.ALPHA,1);
+//        isoMessage.setValue(24, "308339",IsoType.ALPHA,6);
+//        isoMessage.setValue(25, "75.00",IsoType.AMOUNT,10);
+//        isoMessage.setValue(26, "1",IsoType.ALPHA,2);
+//        isoMessage.setValue(27, "75.0000",IsoType.ALPHA,10);
+//        isoMessage.setValue(28, "001",IsoType.ALPHA,3);
+//        isoMessage.setValue(29, "78965",IsoType.ALPHA,7);
+//
+//        
+//        isoMessage.setCharacterEncoding(
+//                    "UTF-8");
+//            isoMessage.setBinaryBitmap(
+//                    true);
+//            byte[] data = isoMessage.writeData();
+//
+//            return new String(data);
+//        } catch (IOException ex) {
+//         //   Logger.getLogger(NBSStubImpl.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        return null;
+//    }
+
 }

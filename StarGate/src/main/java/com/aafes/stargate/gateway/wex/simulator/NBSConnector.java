fe36c4d079dd567c913da7ba813c714e49919450
uikuyr <uikuyr@EC2AMAZ-SKF6SHG.corp.aafes.stargate.com>
2017-08-11 17:49:03 +0530

    Merge origin/master
    Conflicts:
    	StarGate/src/test/java/com/aafes/stargate/gateway/wex/simulator/NBSFormatterTest.java

---
+++ StarGate/src/main/java/com/aafes/stargate/gateway/wex/simulator/NBSConnector.java
@@ -0,0 +1,63 @@
+package com.aafes.stargate.gateway.wex.simulator;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author uikuyr
+ */
+public class NBSConnector {
+
+    private static final Logger LOG = Logger.getLogger(NBSConnector.class.getName());
+    private StringBuffer response;
+    byte[] isoFormat;
+private Socket nbsSocket;
+
+    public byte[] sendRequest(String request) {
+
+        try {
+
+            //creating connections
+            if(null==nbsSocket || nbsSocket.isClosed()){
+            nbsSocket = new Socket("localhost", 2000);
+            LOG.info("Connection Established with NBS socket server");
+            LOG.log(Level.INFO, "{0}-----------{1}---------------{2}", new Object[]{nbsSocket.getClass(), nbsSocket.getLocalAddress(), nbsSocket.getLocalPort()});
+            } 
+
+            BufferedWriter writeRequest
+                    = new BufferedWriter(new OutputStreamWriter(nbsSocket.getOutputStream()));
+
+            writeRequest.write(request + "\n");
+            writeRequest.flush();
+
+            getResponse();
+            nbsSocket.close();
+            isoFormat = response.toString().getBytes();
+            return isoFormat;
+        } catch (IOException ex) {
+            Logger.getLogger(NBSConnector.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("nbsclient.NBSClient.main()" + ex);
+        }
+        return isoFormat;
+    }
+    
+    public void getResponse() throws IOException{
+        String responsePerLine;
+        BufferedReader readResponse
+                    = new BufferedReader(new InputStreamReader(nbsSocket.getInputStream()));
+
+            while ((responsePerLine = readResponse.readLine()) != null) {
+                if(response == null) response = new StringBuffer();
+                response.append(responsePerLine);
+                LOG.log(Level.INFO, "Response recieved as {0}", response);
+            }
+    }
+    
+}

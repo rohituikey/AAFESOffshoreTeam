bc8a1003f38693a16e41a77363aeef282353e5b2
uikuyr <uikuyr@EC2AMAZ-SKF6SHG.corp.aafes.stargate.com>
2017-08-29 17:33:05 +0530

    nbs Maven project

---
+++ NBSServer/src/main/java/WexStub/NBSStub.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WexStub;
+
+/**
+ *
+ * @author uikuyr
+ */
+public interface NBSStub {
+    
+    public String getResponse(String Request);
+//    public String unpackIso8583(String req);
+    
+}

920f7ee00c79b80a5ef4f7ee72b75e5589942599
uikuyr <uikuyr@EC2AMAZ-SKF6SHG.corp.aafes.stargate.com>
2017-08-30 13:37:52 +0530

    removed NBSStub

--- StarGate/src/main/resources/Script/TestShell.sh
+++
@@ -1,9 +0,0 @@
-set path=C:\Program Files\Java\jdk1.8.0_131\bin
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_131
-set classpath=.\standalone\deployments\StarGate-1.0-SNAPSHOT.war\WEB-INF\classes\com\aafes\stargate\gateway\svs
-
-java -cp $classpath\ Scheduler
-
-if [grep -ic "exception" -ne 0]
-
-mail -s "your subject" your@email.com

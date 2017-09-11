8bcc4422219aa90d482aff379fc91bc0b6437c49
uikuyr <uikuyr@EC2AMAZ-SKF6SHG.corp.aafes.stargate.com>
2017-09-11 13:19:37 +0530

    tags replaced by properties.

--- StarGate/src/main/java/com/aafes/stargate/gateway/wex/simulator/NBSFormatterFS.java
+++ StarGate/src/main/java/com/aafes/stargate/gateway/wex/simulator/NBSFormatterFS.java
@@ -30,62 +30,62 @@ public class NBSFormatterFS {
         StringBuilder str = new StringBuilder();
         //LOGON Request
         if (null != t.getTermId() || !t.getTermId().isEmpty()) {
-            str.append("<SX>").append(t.getTermId());//termID
+            str.append(configurator.get("START_TAG")).append(t.getTermId());//termID
         }
-        str.append("<FS>" + ConstantsUtil.APPLICATIONNAME);//APPLICATIONNAME
-        str.append("<FS>" + ConstantsUtil.APPLICATIONVERSION);//APPLICATIONVERSION
-        str.append("<FS>").append(createDateFormat());
+        str.append(configurator.get("FIELD_SEPERATOR") + ConstantsUtil.APPLICATIONNAME);//APPLICATIONNAME
+        str.append(configurator.get("FIELD_SEPERATOR") + ConstantsUtil.APPLICATIONVERSION);//APPLICATIONVERSION
+        str.append(configurator.get("FIELD_SEPERATOR")).append(createDateFormat());
         //ACTUAL AUTH REQUEST STARTS
-        str.append("<FS>" + ConstantsUtil.SESSIONTYPEAUTH);
+        str.append(configurator.get("FIELD_SEPERATOR") + ConstantsUtil.SESSIONTYPEAUTH);
         if (null != t.getTransactionId() || !t.getTransactionId().isEmpty()) {
-            str.append("<FS>").append(t.getTransactionId().substring(0, 4));//key
+            str.append(configurator.get("FIELD_SEPERATOR")).append(t.getTransactionId().substring(0, 4));//key
         }
-        str.append("<FS>" + ConstantsUtil.TRANSTYPEPREAUTH);//transtype
-        str.append("<FS>" + ConstantsUtil.CARDTYPEWEX);//cardType
+        str.append(configurator.get("FIELD_SEPERATOR") + ConstantsUtil.TRANSTYPEPREAUTH);//transtype
+        str.append(configurator.get("FIELD_SEPERATOR") + ConstantsUtil.CARDTYPEWEX);//cardType
         if (null != t.getCatFlag() || !t.getCatFlag().isEmpty()) {
-            str.append("<FS>").append(t.getCatFlag());//catFlag
+            str.append(configurator.get("FIELD_SEPERATOR")).append(t.getCatFlag());//catFlag
         }
         if (null != t.getPumpNmbr() || !t.getPumpNmbr().isEmpty()) {
-            str.append("<FS>").append(t.getPumpNmbr());//pumpNUMBER
+            str.append(configurator.get("FIELD_SEPERATOR")).append(t.getPumpNmbr());//pumpNUMBER
         }
-        str.append("<FS>" + ConstantsUtil.SERVICETYPE);//SERVICETYPE
+        str.append(configurator.get("FIELD_SEPERATOR") + ConstantsUtil.SERVICETYPE);//SERVICETYPE
         if (t.getInputType().equalsIgnoreCase(InputType.SWIPED)) {
-            str.append("<FS>" + ConstantsUtil.TRACKNUMBERWEXTWO);//track
+            str.append(configurator.get("FIELD_SEPERATOR") + ConstantsUtil.TRACKNUMBERWEXTWO);//track
             if (null != t.getTrack2() || !t.getTrack2().isEmpty()) {
-                str.append("<FS>").append(t.getTrack2());//track2
+                str.append(configurator.get("FIELD_SEPERATOR")).append(t.getTrack2());//track2
             }
         }
         if(t.getAmount()>0){
           double d =  t.getAmount()/100;
         DecimalFormat format = new DecimalFormat("0.00");
         String formatted = format.format(d);
-        str.append("<FS>").append(formatted);//amount
+        str.append(configurator.get("FIELD_SEPERATOR")).append(formatted);//amount
         }
-        str.append("<FS>").append(t.getPromptDetailCount().toString());
+        str.append(configurator.get("FIELD_SEPERATOR")).append(t.getPromptDetailCount().toString());
         if (null != t.getVehicleId()&&t.getVehicleId().trim().length()>0 ) {
-            str.append("<FS>" + ConstantsUtil.VEHICLEID);//prompttype
-            str.append("<FS>").append(t.getVehicleId());//promptvalue
+            str.append(configurator.get("FIELD_SEPERATOR") + ConstantsUtil.VEHICLEID);//prompttype
+            str.append(configurator.get("FIELD_SEPERATOR")).append(t.getVehicleId());//promptvalue
         }
         if (null != t.getDriverId() && t.getDriverId().trim().length()>0 ) {
-            str.append("<FS>" + ConstantsUtil.DRIVERID);//prompttype
-            str.append("<FS>").append(t.getDriverId());//promptvalue
+            str.append(configurator.get("FIELD_SEPERATOR") + ConstantsUtil.DRIVERID);//prompttype
+            str.append(configurator.get("FIELD_SEPERATOR")).append(t.getDriverId());//promptvalue
         }
         if (null != t.getOdoMeter() || t.getOdoMeter().trim().length()>0 ) {
-            str.append("<FS>" + ConstantsUtil.ODOMOETER);//prompttype
-            str.append("<FS>").append(t.getOdoMeter());//promptvalue
+            str.append(configurator.get("FIELD_SEPERATOR") + ConstantsUtil.ODOMOETER);//prompttype
+            str.append(configurator.get("FIELD_SEPERATOR")).append(t.getOdoMeter());//promptvalue
         }
-        str.append("<FS>").append(t.getProdDetailCount());//proDetailCount
+        str.append(configurator.get("FIELD_SEPERATOR")).append(t.getProdDetailCount());//proDetailCount
         if (null != t.getProducts() && (t.getProducts().size()) > 0) {
             for (String nonFuelString : t.getProducts()) {
                 if (nonFuelString.contains(ConstantsUtil.PRODUCTDELIMITOR)) {
                     productDetails = nonFuelString.split(ConstantsUtil.PRODUCTDELIMITOR);
-                    str.append("<FS>").append(productDetails[2]);//price
-                    str.append("<FS>").append(productDetails[1]);//quantity
-                    str.append("<FS>").append(productDetails[0]);//prodCode
-                    str.append("<FS>").append(productDetails[3]);//fdAmount
+                    str.append(configurator.get("FIELD_SEPERATOR")).append(productDetails[2]);//price
+                    str.append(configurator.get("FIELD_SEPERATOR")).append(productDetails[1]);//quantity
+                    str.append(configurator.get("FIELD_SEPERATOR")).append(productDetails[0]);//prodCode
+                    str.append(configurator.get("FIELD_SEPERATOR")).append(productDetails[3]);//fdAmount
                 }
             }
-            str.append("<EX><LF>");
+            str.append(configurator.get("END_TAG"));
         }
         return str.toString();
     }
@@ -94,62 +94,62 @@ public class NBSFormatterFS {
        StringBuilder str = new StringBuilder();
         //LOGON Request
         if (null != t.getTermId() || !t.getTermId().isEmpty()) {
-            str.append("<SX>").append(t.getTermId());//termID
+            str.append(configurator.get("START_TAG")).append(t.getTermId());//termID
         }
-        str.append("<FS>" + ConstantsUtil.APPLICATIONNAME);//APPLICATIONNAME
-        str.append("<FS>" + ConstantsUtil.APPLICATIONVERSION);//APPLICATIONVERSION
-        str.append("<FS>").append(createDateFormat());
+        str.append(configurator.get("FIELD_SEPERATOR") + ConstantsUtil.APPLICATIONNAME);//APPLICATIONNAME
+        str.append(configurator.get("FIELD_SEPERATOR") + ConstantsUtil.APPLICATIONVERSION);//APPLICATIONVERSION
+        str.append(configurator.get("FIELD_SEPERATOR")).append(createDateFormat());
         //ACTUAL AUTH REQUEST STARTS
-        str.append("<FS>" + ConstantsUtil.SESSIONTYPEAUTH);
+        str.append(configurator.get("FIELD_SEPERATOR") + ConstantsUtil.SESSIONTYPEAUTH);
         if (null != t.getTransactionId() || !t.getTransactionId().isEmpty()) {
-            str.append("<FS>").append(t.getTransactionId().substring(0, 4));//key
+            str.append(configurator.get("FIELD_SEPERATOR")).append(t.getTransactionId().substring(0, 4));//key
         }
-        str.append("<FS>" + ConstantsUtil.TRANSTYPEPREAUTH);//transtype
-        str.append("<FS>" + ConstantsUtil.CARDTYPEWEX);//cardType
+        str.append(configurator.get("FIELD_SEPERATOR") + ConstantsUtil.TRANSTYPEPREAUTH);//transtype
+        str.append(configurator.get("FIELD_SEPERATOR") + ConstantsUtil.CARDTYPEWEX);//cardType
         if (null != t.getCatFlag() || !t.getCatFlag().isEmpty()) {
-            str.append("<FS>").append(t.getCatFlag());//catFlag
+            str.append(configurator.get("FIELD_SEPERATOR")).append(t.getCatFlag());//catFlag
         }
         if (null != t.getPumpNmbr() || !t.getPumpNmbr().isEmpty()) {
-            str.append("<FS>").append(t.getPumpNmbr());//pumpNUMBER
+            str.append(configurator.get("FIELD_SEPERATOR")).append(t.getPumpNmbr());//pumpNUMBER
         }
-        str.append("<FS>" + ConstantsUtil.SERVICETYPE);//SERVICETYPE
+        str.append(configurator.get("FIELD_SEPERATOR") + ConstantsUtil.SERVICETYPE);//SERVICETYPE
         if (t.getInputType().equalsIgnoreCase(InputType.SWIPED)) {
-            str.append("<FS>" + ConstantsUtil.TRACKNUMBERWEXTWO);//track
+            str.append(configurator.get("FIELD_SEPERATOR") + ConstantsUtil.TRACKNUMBERWEXTWO);//track
             if (null != t.getTrack2() || !t.getTrack2().isEmpty()) {
-                str.append("<FS>").append(t.getTrack2());//track2
+                str.append(configurator.get("FIELD_SEPERATOR")).append(t.getTrack2());//track2
             }
         }
         if(t.getAmount()>0){
           double d =  t.getAmount()/100;
         DecimalFormat format = new DecimalFormat("0.00");
         String formatted = format.format(d);
-        str.append("<FS>").append(formatted);//amount
+        str.append(configurator.get("FIELD_SEPERATOR")).append(formatted);//amount
         }
-        str.append("<FS>").append(t.getPromptDetailCount().toString());
+        str.append(configurator.get("FIELD_SEPERATOR")).append(t.getPromptDetailCount().toString());
         if (null != t.getVehicleId()&&t.getVehicleId().trim().length()>0 ) {
-            str.append("<FS>" + ConstantsUtil.VEHICLEID);//prompttype
-            str.append("<FS>").append(t.getVehicleId());//promptvalue
+            str.append(configurator.get("FIELD_SEPERATOR") + ConstantsUtil.VEHICLEID);//prompttype
+            str.append(configurator.get("FIELD_SEPERATOR")).append(t.getVehicleId());//promptvalue
         }
         if (null != t.getDriverId() && t.getDriverId().trim().length()>0 ) {
-            str.append("<FS>" + ConstantsUtil.DRIVERID);//prompttype
-            str.append("<FS>").append(t.getDriverId());//promptvalue
+            str.append(configurator.get("FIELD_SEPERATOR") + ConstantsUtil.DRIVERID);//prompttype
+            str.append(configurator.get("FIELD_SEPERATOR")).append(t.getDriverId());//promptvalue
         }
         if (null != t.getOdoMeter() || t.getOdoMeter().trim().length()>0 ) {
-            str.append("<FS>" + ConstantsUtil.ODOMOETER);//prompttype
-            str.append("<FS>").append(t.getOdoMeter());//promptvalue
+            str.append(configurator.get("FIELD_SEPERATOR") + ConstantsUtil.ODOMOETER);//prompttype
+            str.append(configurator.get("FIELD_SEPERATOR")).append(t.getOdoMeter());//promptvalue
         }
-        str.append("<FS>").append(t.getProdDetailCount());//proDetailCount
+        str.append(configurator.get("FIELD_SEPERATOR")).append(t.getProdDetailCount());//proDetailCount
         if (null != t.getProducts() && (t.getProducts().size()) > 0) {
             for (String nonFuelString : t.getProducts()) {
                 if (nonFuelString.contains(ConstantsUtil.PRODUCTDELIMITOR)) {
                     productDetails = nonFuelString.split(ConstantsUtil.PRODUCTDELIMITOR);
-                    str.append("<FS>").append(productDetails[2]);//price
-                    str.append("<FS>").append(productDetails[1]);//quantity
-                    str.append("<FS>").append(productDetails[0]);//prodCode
-                    str.append("<FS>").append(productDetails[3]);//fdAmount
+                    str.append(configurator.get("FIELD_SEPERATOR")).append(productDetails[2]);//price
+                    str.append(configurator.get("FIELD_SEPERATOR")).append(productDetails[1]);//quantity
+                    str.append(configurator.get("FIELD_SEPERATOR")).append(productDetails[0]);//prodCode
+                    str.append(configurator.get("FIELD_SEPERATOR")).append(productDetails[3]);//fdAmount
                 }
             }
-            str.append("<EX><LF>");
+            str.append(configurator.get("END_TAG"));
         }
         return str.toString();
     }
@@ -162,4 +162,15 @@ public class NBSFormatterFS {
         ts = ts.substring(11, 13) + ts.substring(14, 16) + ConstantsUtil.DAYLIGHTSAVINGSTIMEATSITEONE;
         return ts;
     }
+
+    public Configurator getConfigurator() {
+        return configurator;
+    }
+
+    public void setConfigurator(Configurator configurator) {
+        this.configurator = configurator;
+    }
+    
+    
+    
 }

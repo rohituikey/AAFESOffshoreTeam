1823a78bff598166ff468c94379a7201172369eb
uikuyr <uikuyr@EC2AMAZ-SKF6SHG.corp.aafes.stargate.com>
2017-11-07 16:11:19 +0530

    Settlement reject service rest project.

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/SettlementRejectService.java
@@ -0,0 +1,37 @@
+package com.mycompany.mavenproject1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/hello")
+public class SettlementRejectService {
+
+    @POST
+    @Path("/name")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getMsg(InputStream input) {
+        StringBuilder response = new StringBuilder();
+        try {
+
+            BufferedReader readInput = new BufferedReader(new InputStreamReader(input));
+            String line = null;
+            while ((line = readInput.readLine()) != null) {
+                response.append(line);
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(SettlementRejectService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return Response.status(200).build();
+    }
+
+}
